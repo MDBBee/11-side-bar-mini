1ffe9883707b0a888e600c46740b72eb18681561
MDBBee <bobbyugbebor@gmail.com>
2024-11-07 09:40:45 +0200

    Sidebar funtionality completed

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,36 @@
+import { PiArrowFatLeftFill } from 'react-icons/pi';
+import logo from './logo.svg';
+import { useGlobalContext } from './context';
+import { links, social } from './data';
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const { isSideBarOpen, closeSideBar } = useGlobalContext();
+  return (
+    <aside className={isSideBarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
+      <div className="sidebar-header">
+        <img src={logo} alt="" className="logo" />
+        <button className="close-btn" onClick={closeSideBar}>
+          <PiArrowFatLeftFill />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => (
+          <li key={link.id}>
+            <a href={link.url}>
+              {link.icon}
+              {link.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => (
+          <li key={link.id}>
+            <a href={link.url}>{link.icon}</a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 export default Sidebar;
